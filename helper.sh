6473287f11cc873b2f7e43bee455a149417ebf4e
Mykola Galushka <mm.galushka@auromind.org>
2021-10-09 15:41:25 +0100

    Added MongoDB helper shell.

--- helper.sh
+++ helper.sh
@@ -9,8 +9,11 @@ BOLD=$(echo "\033[1;39m")
 CMD=$(echo "\033[1;34m")
 OPT=$(echo "\033[0;34m")
 
-action_usage(){
+ENVAR="MONGO_INITDB_ROOT_USERNAME=admin
+MONGO_INITDB_ROOT_PASSWORD=secret
+MONGO_INITDB_LOCATION=temp/db"
 
+action_usage(){
     echo -e "    ____                ______                   "
     echo -e "   / __ \\___  ___  ____/_  __/________  ________ "
     echo -e "  / / / / _ \\/ _ \\/ __ \\/ / / ___/ __ '/ ___/ _ \\"
@@ -19,7 +22,10 @@ action_usage(){
     echo -e "IDENTITY       /_/   Developed by Auromind Ltd."
     echo -e ""                                          
     echo -e "${BOLD}System Commands:${NC}"
-    echo -e "   ${CMD}init${NC} initializers environment;"
+    echo -e "   ${CMD}init${OPT} ...${NC} initializers environment;"
+    echo -e "      ${OPT}-u <USERNAME> ${NC}set MongoDB root user name;"
+    echo -e "      ${OPT}-p <PASSWORD>${NC}set MongoDB root password;"
+    echo -e "      ${OPT}-l <DIR>${NC}set MongoDB files location;"
     echo -e "   ${CMD}test${OPT} ...${NC} runs tests;"
     echo -e "      ${OPT}-m <MARK> ${NC}runs tests for mark;"
     echo -e "      ${OPT}-c ${NC}generates code coverage summary;"
@@ -27,15 +33,61 @@ action_usage(){
 }
 
 action_init(){
-    # if [ -d .venv ];
-    #     then
-    #         rm -r .venv
-    # fi
+    # Setup project environment variables
+    if [ ! -f .envar ]; then
+        while [[ $# -gt 0 ]]; do
+            key="$1"
+            case $key in
+                -u|--username)
+                    MONGO_INITDB_ROOT_USERNAME="$2"
+                    shift # past argument
+                    shift # past value
+                ;;
+                -p|--password)
+                    MONGO_INITDB_ROOT_PASSWORD="$2"
+                    shift # past argument
+                    shift # past value
+                ;;
+                -l|--location)
+                    MONGO_INITDB_LOCATION="$2"
+                    shift # past argument
+                    shift # past value
+                ;;
+                *) # unknown option
+                    echo -e "Invalid option!"
+                    echo  -e "Usage: init [-u USERNAME] [-p PASSWORD] [-l DIR]"
+                    echo -e "Learn more with: ./helper.sh help"
+                    exit
+                ;;
+            esac
+        done
 
-    # python3 -m venv .venv
+        if [ -z "$MONGO_INITDB_ROOT_USERNAME" -a "$MONGO_INITDB_ROOT_USERNAME" = "" ]; then 
+            MONGO_INITDB_ROOT_USERNAME='admin'
+        fi
+        if [ -z "$MONGO_INITDB_ROOT_PASSWORD" -a "$MONGO_INITDB_ROOT_PASSWORD" = "" ]; then 
+            MONGO_INITDB_ROOT_PASSWORD='secret'
+        fi
+        if [ -z "$MONGO_INITDB_LOCATION" -a "$MONGO_INITDB_LOCATION" = "" ]; then 
+            MONGO_INITDB_LOCATION='temp/db'
+        fi
+
+        echo ".envar file not found and will be created!"
+        echo "# The project environment variables" > .envar
+        echo "MONGO_INITDB_ROOT_USERNAME=$MONGO_INITDB_ROOT_USERNAME" >> .envar
+        echo "MONGO_INITDB_ROOT_PASSWORD=$MONGO_INITDB_ROOT_PASSWORD" >> .envar 
+        echo "MONGO_INITDB_LOCATION=$MONGO_INITDB_LOCATION" >> .envar
+    fi
+
+    # Setup Python virtual environment 
+    if [ -d .venv ];
+        then
+            rm -r .venv
+    fi
+
+    python3 -m venv .venv
     source .venv/bin/activate
-    # pip3 install -r requirements.txt --no-cache
-    pip3 install python-multipart
+    pip3 install -r requirements.txt --no-cache
 }
 
 action_test(){
@@ -69,13 +121,56 @@ action_run(){
 }
 
 action_mongo(){
-    sudo docker start $(docker ps -a -q --filter "status=exited")
-    # sudo docker run -d --name mongo \
-    #     -p 27017:27017 \
-    #     -v /media/mykola/Work/workspace/deeptrace/db:/data/db \
-    #     -e MONGO_INITDB_ROOT_USERNAME=admin \
-    #     -e MONGO_INITDB_ROOT_PASSWORD=secret \
-    #     mongo
+    source .envar
+
+    CONTAINER_ID=$(docker ps -q --filter="NAME=mongo")
+
+    case $1 in
+        run)
+            sudo docker run -d --name mongo \
+                -p 27017:27017 \
+                -v $MONGO_INITDB_LOCATION:/data/db \
+                -e MONGO_INITDB_ROOT_USERNAME=$MONGO_INITDB_ROOT_USERNAME \
+                -e MONGO_INITDB_ROOT_PASSWORD=$MONGO_INITDB_ROOT_PASSWORD \
+                mongo
+        ;;
+        status)
+            if [ -z "$CONTAINER_ID" -a "$CONTAINER_ID" = "" ]; then 
+                echo -e "MongoDB is not running..."
+            else
+                echo -e "MongoDB is running ($CONTAINER_ID)..."
+            fi 
+        ;;
+        start)
+            if [ -z "$CONTAINER_ID" -a "$CONTAINER_ID" = "" ]; then 
+                sudo docker start mongo
+            else
+                echo -e "MongoDB is running ($CONTAINER_ID)..."
+            fi 
+        ;;
+        stop)
+            if [ -z "$CONTAINER_ID" -a "$CONTAINER_ID" = "" ]; then 
+                echo -e "MongoDB is not running..."
+            else
+                sudo docker stop $CONTAINER_ID
+            fi 
+        ;;
+        restart)
+            if [ -z "$CONTAINER_ID" -a "$CONTAINER_ID" = "" ]; then 
+                sudo docker start mongo
+            else
+                sudo docker stop $CONTAINER_ID
+                sleep 1s
+                sudo docker start mongo
+            fi 
+        ;;
+        *)
+            echo -e "Invalid command!"
+            echo  -e "Usage: mongo [run|status|start|stop|restart]"
+            echo -e "Learn more with: ./helper.sh help"
+            exit
+        ;;
+    esac  
 }
 
 # =============================================================================
@@ -83,7 +178,7 @@ action_mongo(){
 # =============================================================================
 case $1 in
     init)
-        action_init
+        action_init ${@:2}
     ;;
     test)
         action_test ${@:2}
@@ -92,7 +187,7 @@ case $1 in
         action_run
     ;;
     mongo)
-        action_mongo
+        action_mongo ${@:2}
     ;;
 
     *)
